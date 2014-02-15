78d49da705090573840a754932e8a0f8b9e3ab83
Art Furrer <artfurrer@elfu.de>
2014-02-15 23:28:53 +0100

    finished setting up basic authentication

---
+++ importsample.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/Users/msaupe/Library/Python/2.7/site-packages')
+from apiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
+
+import httplib2
